3ba5adc4c1f1f9738b93e620c0bdd6b8d79ad0f0
Nikodem Wróbel <nixonsparrow@gmail.com>
2021-12-23 13:33:10 +0100

    Add model Rate

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from cars.models import Car, Rate
 
-# Register your models here.
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['id', 'make', 'model']
+
+
+class RateAdmin(admin.ModelAdmin):
+    list_display = ['id', 'car', 'rate']
+
+
+admin.site.register(Car, CarAdmin)
+admin.site.register(Rate, RateAdmin)
--- cars/models.py
+++ cars/models.py
@@ -1,5 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Car(models.Model):
+    make = models.CharField(max_length=50, default='', null=False, blank=False)
     model = models.CharField(max_length=50, default='', null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.make} | {self.model}'
+
+
+class Rate(models.Model):
+    rate = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(5.0)])
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='rates')
