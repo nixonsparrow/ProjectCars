ebc0003e8830d99dad0a1a4734f9dcf7c8111451
Nixon Sparrow <nixonsparrow@gmail.com>
2021-12-23 23:33:33 +0100

    Installed rest_framework

--- ProjectCars/settings.py
+++ ProjectCars/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cars'
+    'rest_framework',
+    'cars',
 ]
 
 MIDDLEWARE = [
