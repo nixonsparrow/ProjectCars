d2c342fd6142587e5a02739b9a4f483047ced3e9
Nikodem Wróbel <nixonsparrow@gmail.com>
2021-12-27 20:06:19 +0100

    config.json SECRET_FILE new dev solution

--- ProjectCars/settings.py
+++ ProjectCars/settings.py
@@ -20,8 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-with open(BASE_DIR / 'config.json') as config_file:
-    config = json.load(config_file)
+try:
+    with open(BASE_DIR / 'config.json') as config_file:
+        config = json.load(config_file)
+except FileNotFoundError:
+    config = {
+        "SECRET_KEY": "don't_use%that#in$production(but)create&config.json*file"
+    }
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config['SECRET_KEY']
