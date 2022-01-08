a7b8c917ce86cc2bc7b2f1cfec74c6d198e52a44
Nikodem Wróbel <nixonsparrow@gmail.com>
2022-01-08 23:17:51 +0100

    Added class views for GET/POST method views

--- cars/urls.py
+++ cars/urls.py
@@ -3,8 +3,8 @@ from cars import views
 
 urlpatterns = [
     path('', views.welcome, name='welcome'),
-    path('cars/', views.car_list, name='car-list'),
+    path('cars/', views.CarListAPI.as_view(), name='car-list'),
     path('cars/<int:pk>/', views.car_detail, name='car-detail'),
-    path('rate/', views.add_rate, name='add-rate'),
-    path('popular/', views.car_popular, name='car-popular'),
+    path('rate/', views.AddRateAPI.as_view(), name='add-rate'),
+    path('popular/', views.CarPopularAPI.as_view(), name='car-popular'),
 ]
--- cars/views.py
+++ cars/views.py
@@ -3,6 +3,7 @@ from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
 from rest_framework.decorators import renderer_classes, api_view
 from rest_framework import status
+from rest_framework import views
 from django.shortcuts import render
 from .models import Car
 from .serializers import CarSerializer, RateSerializer, PopularCarSerializer
@@ -12,15 +13,16 @@ def welcome(request):
     return render(request, 'cars/welcome.html')
 
 
-@api_view(('GET', 'POST'))
-@renderer_classes((JSONRenderer,))
-def car_list(request):
-    if request.method == 'GET':
+class CarListAPI(views.APIView):
+    serializer_class = CarSerializer
+    http_method_names = ['get', 'post']
+
+    def get(self, request):
         cars = Car.objects.all()
         serializer = CarSerializer(cars, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, *arg, **kwargs):
         data = JSONParser().parse(request)
         serializer = CarSerializer(data=data)
         if serializer.is_valid():
@@ -29,10 +31,11 @@ def car_list(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(('GET',))
-@renderer_classes((JSONRenderer,))
-def car_popular(request):
-    if request.method == 'GET':
+class CarPopularAPI(views.APIView):
+    serializer_class = PopularCarSerializer
+    http_method_names = ['get']
+
+    def get(self, request):
         cars = Car.objects.all()
         serializer = PopularCarSerializer(cars, many=True)
         return Response(serializer.data)
@@ -51,10 +54,11 @@ def car_detail(request, pk):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(('POST', ))
-@renderer_classes((JSONRenderer,))
-def add_rate(request):
-    if request.method == 'POST':
+class AddRateAPI(views.APIView):
+    serializer_class = CarSerializer
+    http_method_names = ['post']
+
+    def post(self, request, *arg, **kwargs):
         data = JSONParser().parse(request)
         translated_data = {'car': data['car_id'], 'rate': data['rating']}
         serializer = RateSerializer(data=translated_data)
